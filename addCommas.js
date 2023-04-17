c71c1788c0636c69620bca68d37c77609496c5e7
Ahni Anderson <115118638+ahndawn@users.noreply.github.com>
2023-04-17 13:36:05 -0400

    Add Commas function

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,20 @@
-function addCommas() {}
+function addCommas(num) {
+  // Convert the number to a string
+  let numStr = num.toString();
+  
+  // Use regex (regular expression) to add commas to the number string
+  numStr = numStr.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  
+  // Return the formatted number string
+  return numStr;
+}
+//      \B: This is a negative look-behind assertion that matches a position that is not preceded by a word boundary. 
+//In this case, it ensures that commas are not added at the beginning of the number string.
+
+//      (?=(\d{3})+(?!\d)): This is a positive look-ahead assertion that matches a position followed by one or more groups of three digits
+// not followed by another digit. This effectively identifies the positions where commas need to be added.
+
+//      /g: This is a flag that indicates a global search because it needs to search for all occurrences of the pattern in the input string, rather than just the first occurrence.
+
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -5,3 +5,28 @@ describe("#addCommas", () => {
     expect(typeof addCommas).toBe("function");
   });
 });
+
+// Test case 1: Number with no commas needed
+let num = 123;
+let result = addCommas(num);
+console.log(result); // Output: "123"
+
+// Test case 2: Number with commas needed
+num = 1234567;
+result = addCommas(num);
+console.log(result); // Output: "1,234,567"
+
+// Test case 3: Number with decimals
+num = 1234.5678;
+result = addCommas(num);
+console.log(result); // Output: "1,234.5678"
+
+// Test case 4: Number with negative sign
+num = -12345;
+result = addCommas(num);
+console.log(result); // Output: "-12,345"
+
+// Test case 5: Number with one comma needed
+num = 12345;
+result = addCommas(num);
+console.log(result); // Output: "1,2345"
